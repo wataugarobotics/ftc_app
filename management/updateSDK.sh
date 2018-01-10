b0fde76c004fc122d599dc23c010bc76e1fbed90
benbroce3 <benbroce2@gmail.com>
2018-01-09 23:34:46 -0500

    fix updateSDK script

--- management/updateSDK.sh
+++ management/updateSDK.sh
@@ -4,7 +4,7 @@
 # SDK structure/files should be made, only additions!
 cd ..
 git remote add upstream https://github.com/ftctechnh/ftc_app.git
-git fetch upstream master
+git fetch upstream/master
 git checkout master
 git merge upstream/master
 printf "\nLocal Sync Complete.\nRun 'git push' to update remote.\n"
