0c3b5f1f9a53fcbdd3681adb309aece4479aacfb
benbroce3 <benbroce2@gmail.com>
2018-03-22 02:23:51 -0400

    Update Tank class to allow variable motor number on each side

--- Team5881/src/main/java/org/firstinspires/ftc/team5881/Sumo1/HW_S1.java
+++ Team5881/src/main/java/org/firstinspires/ftc/team5881/Sumo1/HW_S1.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.team5881.Sumo1;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
 public class HW_S1 {
     // OpMode members
@@ -16,7 +15,7 @@ public class HW_S1 {
     // Init hardware interfaces
     public void init(HardwareMap ahwMap) {
         hwMap = ahwMap; // save reference to Hardware map
-        drive = new Tank(hwMap, "mtrL", "mtrR");
+        drive = new Tank(hwMap, new String[] {"mtrL", "mtrR"});
         drive.init(DcMotor.ZeroPowerBehavior.BRAKE, DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
--- Team5881/src/main/java/org/firstinspires/ftc/team5881/Sumo1/Tank.java
+++ Team5881/src/main/java/org/firstinspires/ftc/team5881/Sumo1/Tank.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.team5881.Sumo1;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 
@@ -10,16 +9,11 @@ public class Tank {
     private DcMotor[] motors = null;
 
     /**
-     * Constructs a tank drive with a hwMap & the String names of 2 motors.
+     * Constructs a tank drive with a hwMap and the String names of the drive motors
      *
      * @param hwMap a HardwareMap with which to access the motors
-     * @param mtrL the String name of the Left motor
-     * @param mtrR the String name of the Right motor
+     * @param motors a String[] of the motor names (l, r, l, r)
      */
-    public Tank(HardwareMap hwMap, String mtrL, String mtrR) {
-        this.motors = new DcMotor[] {hwMap.dcMotor.get(mtrL), hwMap.dcMotor.get(mtrR)};
-    }
-
     public Tank(HardwareMap hwMap, String[] motors) {
         this.motors = new DcMotor[motors.length];
         for (int i = 0; i < motors.length; i++) {
